0fda28be1b1fe82b6c498750b3925b9bb689c2fc
Ingo Mohr <tellastory73@gmail.com>
2022-02-12 16:49:19 +0100

    feat: add DurationToStringConverter

---
+++ src/main/java/org/ingomohr/trac/util/DurationToStringConverter.java
@@ -0,0 +1,41 @@
+package org.ingomohr.trac.util;
+
+import java.time.Duration;
+import java.util.Objects;
+
+/**
+ * Converts {@link java.time.Duration} to a string representation.
+ */
+public class DurationToStringConverter {
+
+    /**
+     * Returns a string representation for the given Duration.
+     * 
+     * @param duration the duration. Cannot be <code>null</code>.
+     * @return string representation with info on days, hours and minutes. Never
+     *         <code>null</code>.
+     */
+    public String toString(Duration duration) {
+        Objects.requireNonNull(duration);
+
+        long minutes = duration.toMinutes();
+
+        long hours = minutes / 60;
+        minutes = minutes % 60;
+
+        long days = hours / 24;
+        hours = hours % 24;
+
+        StringBuilder builder = new StringBuilder();
+        if (days > 0) {
+            builder.append(String.format("%,d", days)).append("d ");
+        }
+        if (hours > 0 || days > 0) {
+            builder.append(hours).append("h ");
+        }
+        builder.append(minutes).append("m");
+
+        return builder.toString();
+    }
+
+}
---
+++ src/test/java/org/ingomohr/trac/util/TestDurationToStringConverter.java
@@ -0,0 +1,38 @@
+package org.ingomohr.trac.util;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link DurationToStringConverter}.
+ */
+public class TestDurationToStringConverter {
+
+    private DurationToStringConverter objUT;
+
+    @BeforeEach
+    void prep() {
+        objUT = new DurationToStringConverter();
+    }
+
+    @Test
+    void test() {
+        assertAll(() -> {
+            assertEquals("0m", objUT.toString(Duration.ofMinutes(0)));
+            assertEquals("56m", objUT.toString(Duration.ofMinutes(56)));
+            assertEquals("1h 0m", objUT.toString(Duration.ofMinutes(60)));
+            assertEquals("1h 22m", objUT.toString(Duration.ofMinutes(82)));
+            assertEquals("1d 0h 0m", objUT.toString(Duration.ofMinutes(1440)));
+            assertEquals("1d 1h 0m", objUT.toString(Duration.ofMinutes(1500)));
+            assertEquals("1d 1h 59m", objUT.toString(Duration.ofMinutes(1559)));
+            assertEquals("1.000d 0h 0m", objUT.toString(Duration.ofMinutes(1_000 * 1440)));
+            assertEquals("1.000.000d 0h 0m", objUT.toString(Duration.ofMinutes(1_000_000 * 1440)));
+        });
+    }
+
+}
